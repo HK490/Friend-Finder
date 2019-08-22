980e248f34e1e21edd9b236c6f68c8890250b3e4
Helen Kim <Helenk490@gmail.com>
2019-08-22 17:39:38 -0400

    added background

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,43 +1,43 @@
 const possFriend = [
     {
         "name": "Ruby",
-        "link": "<img src=" + "http://www.f-covers.com/cover/cute-dogs-funny-puppy-puppies-facebook-cover-timeline-banner-for-fb.jpg" + ">",
+        "link": "http://www.f-covers.com/cover/cute-dogs-funny-puppy-puppies-facebook-cover-timeline-banner-for-fb.jpg",
         "ratings": [5,5,5,5,5],
         
     },
     {
         "name": "TomTom",
-        "link": "<img src=" + "http://www.f-covers.com/cover/cute-dogs-funny-puppy-puppies-facebook-cover-timeline-banner-for-fb.jpg" + ">",
+        "link": "https://i.pinimg.com/originals/3c/d2/e7/3cd2e79883436b060f861c20fefbcc66.jpg",
         "ratings": [4,4,4,4,4],
         
     },
     {
         "name": "Tommy",
-        "link": "<img src=" + "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKBvZ_7BFAyVe7rTTillCgpo4no_upoLzrR8Kvaa1zihdSrCag" + ">",
+        "link": "https://i.ytimg.com/vi/kLzpSoW9UdQ/maxresdefault.jpg",
         "ratings": [3,3,3,3,3],
         
     },
     {
         "name": "Milo",
-        "link": "<img src=" + "http://www.f-covers.com/cover/cute-dogs-funny-puppy-puppies-facebook-cover-timeline-banner-for-fb.jpg" + ">",
+        "link": "https://cdn.shopify.com/s/files/1/0066/0121/1973/products/product-image-718872493_580x.jpg?v=1547231814",
         "ratings": [2,3,2,5,2],
         
     },
     {
         "name": "Tuna",
-        "link": "<img src=" + "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKBvZ_7BFAyVe7rTTillCgpo4no_upoLzrR8Kvaa1zihdSrCag" + ">",
+        "link": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKBvZ_7BFAyVe7rTTillCgpo4no_upoLzrR8Kvaa1zihdSrCag",
         "ratings": [4,3,2,1,4],
         
     },
     {
         "name": "Lola",
-        "link": "<img src=" + "http://www.f-covers.com/cover/cute-dogs-funny-puppy-puppies-facebook-cover-timeline-banner-for-fb.jpg" + ">",
+        "link": "https://i.pinimg.com/236x/62/c8/75/62c8758bee59d2a7dae3fbb177d63e3b.jpg",
         "ratings": [5,3,3,4,4],
         
     },
     {
         "name": "Pooh",
-        "link": "<img src=" + "http://www.f-covers.com/cover/cute-dogs-funny-puppy-puppies-facebook-cover-timeline-banner-for-fb.jpg" + ">",
+        "link": "https://i.pinimg.com/originals/3c/d2/e7/3cd2e79883436b060f861c20fefbcc66.jpg",
         "ratings": [1,2,5,5,5],
         
     },
